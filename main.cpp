43eb265a35fffa1cb8af796ff05c9a57aa782e78
Boda-Peter <boda.peter17@gmail.com>
2023-09-20 16:27:43 +0200

    Hibák javításra kerültek

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];                    //Hiányzik az alulvonas
-    std::cout << '1-100 ertekek duplazasa'          //Rossz aposztrof és illetve hianyzik a sor vegen a pontosvesszo
-    for (int i = 0;)                                //Hianyzik a leptetes
+    int *b = new int[N_ELEMENTS];                    //Hiányzik az alulvonas
+    std::cout << "1-100 ertekek duplazasa";          //Rossz aposztrof és illetve hianyzik a sor vegen a pontosvesszo
+    for (int i = 0; i< N_ELEMENTS ; i++)                                //Hianyzik a leptetes
     {
-        b[i] = i * 2;                               //0-tol 99-ig duplaz - 1-tol 100-ig kene
+        b[i] = (i + 1) * 2;                               //0-tol 99-ig duplaz - 1-tol 100-ig kene
     }
-    for (int i = 0; i; i++)                         //Hibas ciklusvezerlo mindig hamisra fut ezert nen lep be a ciklusba
+    for (int i = 0; i < N_ELEMENTS; i++)                         //Hibas ciklusvezerlo mindig hamisra fut ezert nen lep be a ciklusba
     {
-        std::cout << "Ertek:"                       //Ertek kiirasa illetve a pontosvesszo hianyzik
+        std::cout << "Ertek:" << b[i] << std::endl;                    //Ertek kiirasa illetve a pontosvesszo hianyzik
     }    
     std::cout << "Atlag szamitasa: " << std::endl;      
-    int atlag;                                      //Nincs kezdoerteke a valtozonak
-    for (int i = 0; i < N_ELEMENTS, i++)            //Hianyzik a pontosvesszo
+    int atlag = 0;                                      //Nincs kezdoerteke a valtozonak
+    for (int i = 0; i < N_ELEMENTS; i++)            //Hianyzik a pontosvesszo
     {
         atlag += b[i]                               //Hianyzik a pontosvesszo
     }
